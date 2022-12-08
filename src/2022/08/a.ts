d34a710102539ad4c8409afae53dfc367cb8b949
Aaron Stockton <stocad@stocad.com>
2022-12-08 09:14:05 -0600

    Day 8 solution and prep for all future days

--- src/2022/08/a.ts
+++ src/2022/08/a.ts
@@ -4,4 +4,32 @@ const lines: string[] = forFileLinesOf<string>(__dirname, 'input', (line: string
     return line;
 });
 
-console.log("Not started")
+const map = lines.map((l) => l.split('').map((e) => parseInt(e)))
+
+function visibleInDirectionCheck (x: number, y: number, incX: number, incY: number, value: number): boolean {
+    // If we're out of bounds, then we're visible
+    const curValue = map[y]?.[x];
+    if (curValue === undefined) { // Check length bounds correctness
+        return true
+    }
+    if (value <= curValue) {
+        return false
+    }
+    return visibleInDirectionCheck(x + incX, y + incY, incX, incY, value);
+}
+
+function visible (x: number, y: number): boolean {
+    if (
+        visibleInDirectionCheck(x - 1, y, -1, 0, map[y][x]) ||
+        visibleInDirectionCheck(x + 1, y, 1, 0, map[y][x]) ||
+        visibleInDirectionCheck(x, y - 1, 0, -1, map[y][x]) ||
+        visibleInDirectionCheck(x, y + 1, 0, +1, map[y][x])
+    ) {
+        return true;
+    }
+    return false;
+}
+
+const result = map.flatMap((row, y) => row.map((_, x) => visible(x, y)))
+
+console.log(result.reduce((ret, v) => ret + (v ? 1 : 0), 0))
--- src/2022/08/b.ts
+++ src/2022/08/b.ts
@@ -4,4 +4,29 @@ const lines: string[] = forFileLinesOf<string>(__dirname, 'input', (line: string
     return line;
 });
 
-console.log("Not started")
+const map = lines.map((l) => l.split('').map((e) => parseInt(e)))
+
+function visibleInDirection (x: number, y: number, incX: number, incY: number, value: number): number {
+    // If we're out of bounds, then we're visible
+    const curValue = map[y]?.[x];
+    if (curValue === undefined) { // Check length bounds correctness
+        return 0
+    }
+    if (value <= curValue) {
+        return 1
+    }
+    return visibleInDirection(x + incX, y + incY, incX, incY, value) + 1;
+}
+
+function scenicScore (x: number, y: number): number {
+    return (
+        visibleInDirection(x - 1, y, -1, 0, map[y][x]) *
+        visibleInDirection(x + 1, y, 1, 0, map[y][x]) *
+        visibleInDirection(x, y - 1, 0, -1, map[y][x]) *
+        visibleInDirection(x, y + 1, 0, +1, map[y][x])
+    );
+}
+
+const result = map.flatMap((row, y) => row.map((_, x) => scenicScore(x, y)))
+
+console.log(Math.max(...result))
