0f7c00cd0ba9c299628fc7ba50761aa8c59f8340
Aaron Stockton <stocad@stocad.com>
2022-12-10 19:28:52 -0600

    Day 10 solution

--- src/2022/10/a.ts
+++ src/2022/10/a.ts
@@ -4,4 +4,38 @@ const lines: string[] = forFileLinesOf<string>(__dirname, 'input', (line: string
     return line;
 });
 
-console.log("Not started")
+const commands: [string, number | undefined][] = lines.map((l) => {
+    const [cmd, v] = l.split(' ');
+    return [cmd, (v ? parseInt(v) : undefined)]
+})
+
+function getCommand (): [string | undefined, number, number | undefined] {
+    const [command, value] = commands.shift() || [];
+    let duration: number;
+    if (command === 'noop') {
+        duration = 1;
+    } else {
+        duration = 2;
+    }
+    return [command, duration, value]
+}
+
+let [nextCommand, duration, value] = getCommand();
+
+let xRegister = 1;
+
+const tickValues: number[] = []
+
+while (nextCommand) {
+    duration -= 1
+    if (duration === 0) {
+        if (value) {
+            xRegister += value;
+        }
+        [nextCommand, duration, value] = getCommand();
+    }
+    tickValues.push(xRegister)
+}
+
+const ticks = [20, 60, 100, 140, 180, 220];
+console.log(ticks.map((n) => tickValues[n - 2] * n).reduce((r, v) => r + v, 0))
--- src/2022/10/b.ts
+++ src/2022/10/b.ts
@@ -4,4 +4,46 @@ const lines: string[] = forFileLinesOf<string>(__dirname, 'input', (line: string
     return line;
 });
 
-console.log("Not started")
+const commands: [string, number | undefined][] = lines.map((l) => {
+    const [cmd, v] = l.split(' ');
+    return [cmd, (v ? parseInt(v) : undefined)]
+})
+
+function getCommand (): [string | undefined, number, number | undefined] {
+    const [command, value] = commands.shift() || [];
+    let duration: number;
+    if (command === 'noop') {
+        duration = 1;
+    } else {
+        duration = 2;
+    }
+    return [command, duration, value]
+}
+
+let [nextCommand, duration, value] = getCommand();
+
+let xRegister = 1;
+
+const tickValues: number[] = []
+
+while (nextCommand) {
+    tickValues.push(xRegister)
+    duration -= 1
+    if (duration === 0) {
+        if (value) {
+            xRegister += value;
+        }
+        [nextCommand, duration, value] = getCommand();
+    }
+}
+tickValues.push(xRegister)
+
+const pixels = tickValues.map((xVal, i) => {
+    return (xVal - i % 40 === 0 || Math.abs(xVal - i % 40) === 1 ? '#' : '.')
+});
+
+const screenWidth = 40;
+for (let i = 0; i < pixels.length; i += screenWidth) {
+    const row = pixels.slice(i, i + screenWidth).join('');
+    console.log(row)
+}
